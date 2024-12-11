cf9783ca29318311d0243f7f26463f6035021d86
shailesh-stha <shailesh.shrestha049@gmail.com>
2024-12-11 17:42:12 +0100

    updates 12.11

--- source/conf.py
+++ source/conf.py
@@ -29,3 +29,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_css_files = ['css/custom.css']
+html_logo = "_static/coklimax_logo_new.png"
+
+# html_theme_options = {
+#     'logo_only': False,
+#     'display_version': False,
+# }
